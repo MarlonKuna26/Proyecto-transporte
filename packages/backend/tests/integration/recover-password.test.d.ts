19ec3c79b46d6c6643d7f5b32e5a99a3cb6b4224
Heidi <heidimanobanda@gmail.com>
2026-05-06 12:16:52 -0500

    PruebasFuncionando

---
+++ packages/backend/tests/integration/recover-password.test.d.ts
@@ -0,0 +1,1 @@
+//# sourceMappingURL=recover-password.test.d.ts.map
---
+++ packages/backend/tests/integration/recover-password.test.js
@@ -0,0 +1,3 @@
+"use strict";
+// recover-password.test.ts - Prueba: Recuperar contraseña (Integración) - contenido pendiente
+//# sourceMappingURL=recover-password.test.js.map
