398316d60d2bab13ac848a71a1a97322aa51cf13
Heidi <heidimanobanda@gmail.com>
2026-06-06 04:27:31 -0500

    Pruebas

--- packages/backend/jest.integration.config.js
+++ packages/backend/jest.integration.config.js
@@ -12,5 +12,21 @@ module.exports = {
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
     '^@config/(.*)$': '<rootDir>/src/config/$1',
     '^@modules/(.*)$': '<rootDir>/src/modules/$1'
+  },
+  testEnvironmentOptions: {},
+  globalSetup: undefined,
+  setupFiles: [],
+  globals: {
+    'process.env': {
+      DB_HOST: 'u-ride-db',
+      DB_PORT: '5432',
+      DB_USER: 'postgres',
+      DB_PASSWORD: '182004',
+      DB_NAME: 'u_ride_esp',
+      NODE_ENV: 'test',
+      JWT_SECRET: 'test_secret',
+      JWT_REFRESH_SECRET: 'test_refresh_secret',
+      EMAIL_DEV_MODE: 'true'
+    }
   }
 };
