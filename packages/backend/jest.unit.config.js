55724b1a52ed9b571986fb8429be283941181485
Heidi <heidimanobanda@gmail.com>
2026-06-06 10:56:12 -0500

    Cambio prueba unitaria

--- packages/backend/jest.unit.config.js
+++ packages/backend/jest.unit.config.js
@@ -1,32 +1,20 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/tests/unit'],
   testMatch: ['**/*.test.ts'],
-
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
   },
-
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
     '^@config/(.*)$': '<rootDir>/src/config/$1',
     '^@modules/(.*)$': '<rootDir>/src/modules/$1'
   },
-
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-
-  setupFilesAfterEnv: ['<rootDir>/jest.unit.setup.js'],
-
-  moduleDirectories: ['node_modules', 'src'],
-
+  setupFiles: ['<rootDir>/jest.unit.setup.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text', 'clover'],
-
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true,
+  coverageReporters: ['lcov', 'text', 'clover']
 };
